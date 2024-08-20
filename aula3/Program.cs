9a7619f06e7bee3ec6071f903600fe5cc6901dc5
Henrique Bossle <163010167+HenriqueBossle@users.noreply.github.com>
2024-08-20 20:33:22 -0300

    aula3

---
+++ aula3/Program.cs
@@ -0,0 +1,60 @@
+﻿/*int idade = 18;
+
+if(idade >= 18){
+    Console.WriteLine("Você é maior de idade!");
+}
+
+else{
+    Console.WriteLine("Você é menor de idade");
+}
+
+int nota = 85;
+
+if(nota >= 90){
+    Console.WriteLine("Aprovado com excelência!");
+}
+
+else if(nota >= 60){
+    Console.WriteLine("Aprovado!");
+}
+
+else{
+    Console.WriteLine("Reprovado!");
+}
+
+//switch
+
+string cor = "vermelho";
+
+switch (cor){
+    case "vermelho":
+    Console.WriteLine("A cor é vermelha");
+    break;
+    case "azul":
+    Console.WriteLine("A cor é azul");
+    break;
+    default:
+    Console.WriteLine("Cor não reconhecida");
+    break;
+}
+
+for(int i = 0; i <= 5; i++){
+    Console.WriteLine($"Contador: {i}");
+}
+
+int contador = 0;
+while(contador <= 5){
+    Console.WriteLine($"Contador: {contador}");
+    contador ++;
+}
+
+int contador = 0;
+do{
+    contador ++;
+    if(contador % 2 == 0){
+        continue;
+    }
+    Console.WriteLine($"Contador: {contador}");
+} while(contador <= 5); */
+
+
