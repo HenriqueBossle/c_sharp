0b90974d8caa954753482be3b0418c9459459712
Henrique Bossle <163010167+HenriqueBossle@users.noreply.github.com>
2024-08-27 21:34:06 -0300

    Calculadora

---
+++ calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿Calculadora calc = new Calculadora();
+
+Console.WriteLine("Informe o valor de x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+double ResultadoSoma = calc.Soma(x, y);
+Console.WriteLine($"O resultado da soma de {x} e {y} é {ResultadoSoma}");
+
+Console.WriteLine($"O resultado da subtração é {calc.Subtracao(x, y)}");
+
+Console.WriteLine($"O resultado da multiplicação é {calc.Multiplicacao(x, y)}");
+
+Console.WriteLine($"O resultado da divisão é {calc.Divisao(x, y)}");
