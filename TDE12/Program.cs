ce7ceff2bf69b6931e47394e6458858c2e4fe6e1
Henrique Bossle <163010167+HenriqueBossle@users.noreply.github.com>
2024-10-08 21:37:49 -0300

    TDE 12

---
+++ TDE12/Program.cs
@@ -0,0 +1,10 @@
+﻿Empresa empresa = new Empresa();
+Funcionario f1 = new Funcionario("James", 19);
+Gerente gerente = new Gerente("Henrique", 19, 5000.0);
+Estagiario estagiario = new Estagiario("Jonas", 30);
+
+empresa.AdicionarFuncionario(f1);
+empresa.AdicionarFuncionario(gerente);
+empresa.AdicionarFuncionario(estagiario);
+
+empresa.ListarFuncionarios();
