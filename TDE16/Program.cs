c2fcab047287314822db5e4577541a142d4df1de
Henrique Bossle <163010167+HenriqueBossle@users.noreply.github.com>
2024-10-29 21:23:32 -0300

    TDE da aula de hoje

---
+++ TDE16/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(39));
+
+Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(102.2));
+
+Configuracoes.NomeAplicacao = "LP";
+Configuracoes.Versao = 5;
+
+Console.WriteLine(Configuracoes.NomeAplicacao);
+
+Console.WriteLine(Configuracoes.Versao);
